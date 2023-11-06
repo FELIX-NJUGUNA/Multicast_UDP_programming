f12c481fd78ce2407e3ec572bfe94ab328e80a40
felix <felixnjuguna443@gmail.com>
2023-11-06 20:15:57 +0300

    UDP multicast with an input using python

--- client2.py
+++ client2.py
@@ -1,18 +1,40 @@
 import socket
 import struct
 
-cast_grp = '224.1.1.1'
+cast_grp = socket.gethostbyname('224.1.1.1')
 cast_port = 15000
 
+# Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-
+# Bind the socket to the multicast group and port
 sock.bind(('', cast_port))
-mreq = struct.pack("4sl", socket.inet_aton(cast_grp), socket.INADDR_ANY)
 
+# Joins the multicast group
+mreq = struct.pack("4sl", socket.inet_aton(cast_grp), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-while True:
-    print(sock.recv(1024))
+# Make the socket non-blocking
+sock.setblocking(True)
 
+# Disable loopback for the multicast group
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
+
+while True:
+    # creates a datagram packet and sends over the socket
+    try:
+        data, addr = sock.recvfrom(1024)
+    except socket.error:
+        # No data available to receive
+        continue
+
+    # Decode the received data
+    msg = data.decode()
+
+    # Print the received message
+    print(f"{addr}:{msg}")
+    break
+
+# Close the socket
+sock.close()
--- server.py
+++ server.py
@@ -4,12 +4,23 @@ group = '224.1.1.1'
 port = 15000
 
 
-# 2-hop restriction in the network
+# for all packets sent, after two hops on the network the packet will not be re-sent/broadcast
 ttl = 2
 
+# Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
+# Make the socket non-blocking
+sock.setblocking(True)
+
+# Set the multicast TTL
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-sock.sendto(b"Hello Felix", (group, port))
+# Get input from the user
+input_msg = input("Enter your message: ")
+
+# Sends the packet/message
+sock.sendto(input_msg.encode(), (group, port))
+
+sock.close()
 
